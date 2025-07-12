39048027b26f1619fda1ccd905f06f606087e43c
Potato <sofia17.tb@gmail.com>
2025-07-12 17:10:56 -0300

    New entities

---
+++ src/main/java/net/potato_modding/potatospells/compat/Mobs/ISS/Ice_Spider.java
@@ -0,0 +1,102 @@
+package net.potato_modding.potatospells.compat.Mobs.ISS;
+
+import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
+import net.minecraft.core.Holder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.potato_modding.potatospells.utils.PotatoTags;
+
+import static net.potato_modding.potatospells.utils.ConfigFormulas.*;
+
+@SuppressWarnings("unused")
+@EventBusSubscriber
+public class Ice_Spider {
+
+    @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOWEST)
+    private static void handleResistanceAttributeCataclysm(EntityJoinLevelEvent event) {
+        var mob = event.getEntity();
+
+            // Amethyst  attributes
+            Armor += 12 * (1 + m/3.25);
+            Tough += 5 * (1 + m/3.25);
+            Attack += 8.5 * (1 + m/3.25);
+            Resist += 0.8 * m;
+            FireRes -= 0.45 * m;
+            NatRes += 1.25 * m;
+            EndRes += 1.45 * m;
+            BldRes += 0.75 * m;
+            IceRes += 1.85 * m;
+            LigRes += 1.3 * m;
+            EldRes += 1.4 * m;
+            HolyRes += 1.15 * m;
+            BladeRes += 0.9 * m;
+            AbyssRes += 1.55 * m;
+            SoundRes += 1.1 * m;
+            WindRes += 1.6 * m;
+
+        if (mob.getType().is(PotatoTags.ICE_SPIDER)) {
+            setIfNonNull((LivingEntity) mob, Attributes.ARMOR, Armor);
+            setIfNonNull((LivingEntity) mob, Attributes.ARMOR_TOUGHNESS, Tough);
+            setIfNonNull((LivingEntity) mob, Attributes.ATTACK_DAMAGE, Attack);
+            setIfNonNull((LivingEntity) mob, AttributeRegistry.SPELL_RESIST, Resist);
+            setIfNonNull((LivingEntity) mob, AttributeRegistry.FIRE_MAGIC_RESIST, FireRes);
+            setIfNonNull((LivingEntity) mob, AttributeRegistry.NATURE_MAGIC_RESIST, NatRes);
+            setIfNonNull((LivingEntity) mob, AttributeRegistry.ENDER_MAGIC_RESIST, EndRes);
+            setIfNonNull((LivingEntity) mob, AttributeRegistry.BLOOD_MAGIC_RESIST, BldRes);
+            setIfNonNull((LivingEntity) mob, AttributeRegistry.ICE_MAGIC_RESIST, IceRes);
+            setIfNonNull((LivingEntity) mob, AttributeRegistry.LIGHTNING_MAGIC_RESIST, LigRes);
+            setIfNonNull((LivingEntity) mob, AttributeRegistry.ELDRITCH_MAGIC_RESIST, EldRes);
+            setIfNonNull((LivingEntity) mob, AttributeRegistry.HOLY_MAGIC_RESIST, HolyRes);
+            // This needs to be conditional or the game shits itself if the mod is not present
+            if (ModList.get().isLoaded("endersequipment")) {
+                setIfNonNull((LivingEntity) mob, net.ender.endersequipment.registries.EEAttributeRegistry.BLADE_MAGIC_RESIST, BladeRes);
+            }
+            if (ModList.get().isLoaded("cataclysm_spellbooks")) {
+                setIfNonNull((LivingEntity) mob, net.acetheeldritchking.cataclysm_spellbooks.registries.CSAttributeRegistry.ABYSSAL_MAGIC_RESIST, AbyssRes);
+            }
+            if (ModList.get().isLoaded("alshanex_familiars")) {
+                setIfNonNull((LivingEntity) mob, net.alshanex.alshanex_familiars.registry.AttributeRegistry.SOUND_MAGIC_RESIST, SoundRes);
+            }
+            if (ModList.get().isLoaded("aero_additions")) {
+                setIfNonNull((LivingEntity) mob, com.snackpirate.aeromancy.spells.AASpells.Attributes.WIND_MAGIC_RESIST, WindRes);
+            }
+        }
+
+        // We reset this stuff so it doesn't make other mobs go crazy
+        {
+            SpellPower = 0;
+            SchoolPower = 0;
+            Resist = 0;
+            FireRes = 0;
+            IceRes = 0;
+            HolyRes = 0;
+            NatRes = 0;
+            BldRes = 0;
+            EndRes = 0;
+            LigRes = 0;
+            EldRes = 0;
+            AbyssRes = 0;
+            BladeRes = 0;
+            SoundRes = 0;
+            WindRes = 0;
+            Armor = 0;
+            Tough = 0;
+            Attack = 0;
+        }
+    }
+
+    // Actually sets the attributes
+    private static void setIfNonNull(LivingEntity entity, Holder<Attribute> attribute, double value)
+    {
+        var instance = entity.getAttributes().getInstance(attribute);
+        if (instance != null)
+        {
+            instance.setBaseValue(value);
+        }
+    }
+}
